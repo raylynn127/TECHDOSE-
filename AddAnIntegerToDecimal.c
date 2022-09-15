a36d1c55cb73e35f0604b3f8743ab4f2c15163f2
raylynn127 <113158175+raylynn127@users.noreply.github.com>
2022-09-15 10:17:18 +0530

    Add files via upload

---
+++ AddAnIntegerToDecimal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+    int a;
+    float b;
+    printf("ENTER AN INTEGER");
+    scanf("%d",&a);
+    printf("ENTER A DECIMAL VALUE");
+    scanf("%f",&b);
+    printf("THE RESULTANT VALUE IS %.2f",a+b);
+    return 0;
+}
